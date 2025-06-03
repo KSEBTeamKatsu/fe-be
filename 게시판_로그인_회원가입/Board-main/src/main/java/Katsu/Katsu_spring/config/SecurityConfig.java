cfee5fde91930657e4e5e33f9b1a0d58b5b5eba3
LHE <mekakusira@gmail.com>
2025-06-03 21:32:37 +0900

    6.3. degugging

--- 게시판_로그인_회원가입/Board-main/src/main/java/Katsu/Katsu_spring/config/SecurityConfig.java
+++ 게시판_로그인_회원가입/Board-main/src/main/java/Katsu/Katsu_spring/config/SecurityConfig.java
@@ -1,18 +1,48 @@
 package Katsu.Katsu_spring.config;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors((cors) -> cors
+                        .configurationSource(new CorsConfigurationSource() {
+                            @Override
+                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                                CorsConfiguration configuration = new CorsConfiguration();
+                                configuration.setAllowedOrigins(List.of(
+                                        "http://localhost:5173"                                ));
+
+                                configuration.setAllowedMethods(
+                                        List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                                );
+                                configuration.setAllowedHeaders(Collections.singletonList("*"));
+                                configuration.setMaxAge(3600L);
+                                configuration.setAllowCredentials(true);
+                                configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                                return configuration;
+                            }
+                        })
+                )
                 .csrf(csrf -> csrf.disable()) // CSRF 보호 끔 (폼 테스트용일 때)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/login", "/posts", "/css/**", "/api/login").permitAll() // 여기서 /posts 허용!
